01b2a166eb078affaa750ab72e2d44320a246741
Matt McCorry <mccorry@gmail.com>
2024-09-05 21:07:06 +0100

    add build

---
+++ src/libs/cards.ts
@@ -0,0 +1,8 @@
+
+
+
+export const cards = [{ shop: "Tesco", number: "abc" }, { shop: "Nectar", number: "123" }]
+
+export interface Card {
+    shop: string, number: string
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,26 @@
-import React from "react";
-import { Columns } from "react-bulma-components";
+import React, { useState } from "react";
+import { Columns, Dropdown, Level } from "react-bulma-components";
 
 import Layout from "../components/Layout";
 import SEO from "../components/Head";
+import { Card, cards } from "../libs/cards";
+import QRCode from "react-qr-code";
 
-export const Head = () => <SEO title="Matthew McCorry CV" />;
+export const Head = () => <SEO title="Borrow my Clubcard" />;
 
 const UsingTypescript: React.FC = () => {
+  const [card, setCard] = useState<Card>();
   return (
     <Layout>
-      <Columns>
-        <Columns.Column size={3}>
-        </Columns.Column>
-        <Columns.Column>
-        </Columns.Column>
-      </Columns>
+      <Level>
+        Supermarkets now often offer discounts to card members. Left it at home?  Borrow a card for your shop today and get the prices you deserve.
+        <Dropdown onChange={(v: Card) => setCard(v)}>
+          {cards.map(c => <Dropdown.Item value={c}>{c.shop}</Dropdown.Item>)}
+        </Dropdown>
+
+      </Level>
+      {card ? <QRCode value={card?.number || ''}></QRCode> : null}
+
     </Layout>
   );
 };
